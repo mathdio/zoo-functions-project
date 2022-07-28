fed6c2efa0c99b44b1d39c840b19170013099475
matheus <mathdiogenes@gmail.com>
2022-07-28 19:16:34 -0300

    Requisitos 7 e 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((acc, entrant) => {
+    if (entrant.age < 18) {
+      acc.child += 1;
+      return acc;
+    }
+    if (entrant.age >= 18 && entrant.age < 50) {
+      acc.adult += 1;
+      return acc;
+    }
+    acc.senior += 1;
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entrantObject = countEntrants(entrants);
+  const amount = (entrantObject.child * data.prices.child)
+    + (entrantObject.adult * data.prices.adult)
+    + (entrantObject.senior * data.prices.senior);
+  return amount;
 }
 
 module.exports = { calculateEntry, countEntrants };
