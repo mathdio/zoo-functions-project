0a68cce65b5006c58bdb6269d1e660fc91a830ff
matheus <mathdiogenes@gmail.com>
2022-07-28 17:25:43 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  return data.species.reduce((acc, specie) => {
+    const { name, residents } = specie;
+    if (animal === undefined) {
+      acc[name] = residents.length;
+      return acc;
+    }
+    if (name === animal.specie) {
+      if (animal.sex !== undefined) {
+        return residents.filter((resident) => resident.sex === animal.sex).length;
+      }
+      return residents.length;
+    }
+    return acc;
+  }, {});
 }
 
 module.exports = countAnimals;
