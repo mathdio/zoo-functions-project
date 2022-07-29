5f83dc73500caddc5609ee0c6b6c681b65904916
matheus <mathdiogenes@gmail.com>
2022-07-29 19:20:04 -0300

    Requisito 14 incompleto

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined || options.includeNames === undefined) {
+    return {
+      NE: data.species.filter((specie) => specie.location === 'NE').map((specie) => specie.name),
+      NW: data.species.filter((specie) => specie.location === 'NW').map((specie) => specie.name),
+      SE: data.species.filter((specie) => specie.location === 'SE').map((specie) => specie.name),
+      SW: data.species.filter((specie) => specie.location === 'SW').map((specie) => specie.name),
+    };
+  }
+  return {
+    NE: data.species.filter((specie) => specie.location === 'NE').reduce((acc, specie) => {
+      const { name } = specie;
+      const animal = {
+        [name]: specie.residents.filter((resident) => resident.sex === options.sex).map((resident) => resident.name),
+      };
+      acc.push(animal);
+      return acc;
+    }, []),
+  };
 }
+console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
 
 module.exports = getAnimalMap;
