9773c05c1097d5ba0a0ec73eee9ba5225f3863e9
matheus <mathdiogenes@gmail.com>
2022-07-28 21:35:16 -0300

    Requisito 10 (req. 9 incompleto)

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const oldest = species.find((specie) =>
+    specie.id === firstSpecie).residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(oldest);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  // const { Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Monday } = data.hours
+  if (scheduleTarget.endsWith('day')) {
+    const schedule = {};
+    // const animals = data.species.filter((specie) => specie.availability.includes(scheduleTarget)).name;
+    schedule[scheduleTarget] = {
+      officeHour:
+      `Open from ${data.hours[scheduleTarget].open}am until ${data.hours[scheduleTarget].close}pm`,
+      exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+    };
+    console.log(schedule);
+    return schedule;
+  }
+  return data.species.find((specie) => specie.name === scheduleTarget).availability;
 }
+
+console.log(getSchedule('Tuesday'));
 
 module.exports = getSchedule;
