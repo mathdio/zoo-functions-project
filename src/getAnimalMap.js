23742a5631b28f685bd58330a9f65b013a10ac5c
matheus <mathdiogenes@gmail.com>
2022-07-30 18:33:17 -0300

    Requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,5 +1,47 @@
 const data = require('../data/zoo_data');
 
+function mapWithoutSex(options, location) {
+  return data.species.filter((specie) => specie.location === location)
+    .reduce((acc, specie) => {
+      const { name, residents } = specie;
+      const notSorted = residents.map((resident) => resident.name);
+      if (options.sorted) {
+        const sorted = notSorted.sort();
+        const animal = { [name]: sorted };
+        acc.push(animal);
+        return acc;
+      }
+      const animal = { [name]: notSorted };
+      acc.push(animal);
+      return acc;
+    }, []);
+}
+
+function mapWithSex(options, location) {
+  return data.species.filter((specie) => specie.location === location)
+    .reduce((acc, specie) => {
+      const { name, residents } = specie;
+      const notSorted = residents.filter((resident) => resident.sex === options.sex)
+        .map((resident) => resident.name);
+      if (options.sorted) {
+        const sorted = notSorted.sort();
+        const animal = { [name]: sorted };
+        acc.push(animal);
+        return acc;
+      }
+      const animal = { [name]: notSorted };
+      acc.push(animal);
+      return acc;
+    }, []);
+}
+
+function getMap(options, location) {
+  if (options.sex === undefined) {
+    return mapWithoutSex(options, location);
+  }
+  return mapWithSex(options, location);
+}
+
 function getAnimalMap(options) {
   if (options === undefined || options.includeNames === undefined) {
     return {
@@ -10,16 +52,11 @@ function getAnimalMap(options) {
     };
   }
   return {
-    NE: data.species.filter((specie) => specie.location === 'NE').reduce((acc, specie) => {
-      const { name } = specie;
-      const animal = {
-        [name]: specie.residents.filter((resident) => resident.sex === options.sex).map((resident) => resident.name),
-      };
-      acc.push(animal);
-      return acc;
-    }, []),
+    NE: getMap(options, 'NE'),
+    NW: getMap(options, 'NW'),
+    SE: getMap(options, 'SE'),
+    SW: getMap(options, 'SW'),
   };
 }
-console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
 
 module.exports = getAnimalMap;
