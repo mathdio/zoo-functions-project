cac058a78644e62705c44cabb834b1c4ceefc689
matheus <mathdiogenes@gmail.com>
2022-08-01 17:51:52 -0300

    Requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,17 +1,51 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // const { Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Monday } = data.hours
-  if (scheduleTarget.endsWith('day')) {
-    const schedule = {};
-    // const animals = data.species.filter((specie) => specie.availability.includes(scheduleTarget)).name;
-    schedule[scheduleTarget] = {
-      officeHour:
-      `Open from ${data.hours[scheduleTarget].open}am until ${data.hours[scheduleTarget].close}pm`,
-      exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+const arrayWeekDays = ['Tuesday', 'Wednesday',
+  'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+const arrayAnimals = ['lions',
+  'tigers', 'bears', 'penguins', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'];
+
+function getDaySchedule(scheduleTarget) {
+  if (scheduleTarget === 'Monday') {
+    return {
+      Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
     };
-    console.log(schedule);
-    return schedule;
+  }
+  return {
+    [scheduleTarget]: {
+      officeHour: `Open from ${data.hours[scheduleTarget]
+        .open}am until ${data.hours[scheduleTarget].close}pm`,
+      exhibition: data.species.filter((specie) =>
+        specie.availability.includes(scheduleTarget)).map((specie) => specie.name),
+    },
+  };
+}
+
+function getWeekSchedule() {
+  const weekSchedule = {};
+  arrayWeekDays.forEach((day) => {
+    if (day === 'Monday') {
+      weekSchedule[day] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    } else {
+      weekSchedule[day] = {
+        officeHour: `Open from ${data.hours[day]
+          .open}am until ${data.hours[day].close}pm`,
+        exhibition: data.species.filter((specie) =>
+          specie.availability.includes(day)).map((specie) => specie.name),
+      };
+    }
+  });
+  return weekSchedule;
+}
+
+function getSchedule(scheduleTarget) {
+  if (arrayWeekDays.includes(scheduleTarget)) {
+    return getDaySchedule(scheduleTarget);
+  }
+  if (scheduleTarget === undefined
+    || (!arrayAnimals.includes(scheduleTarget) && !arrayWeekDays.includes(scheduleTarget))) {
+    return getWeekSchedule();
   }
   return data.species.find((specie) => specie.name === scheduleTarget).availability;
 }
